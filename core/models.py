1ebf53da00a925a512e452bae4c900acd72adf58
rkmaduka <rkmaduka@quinnipiac.edu>
2015-10-26 21:34:58 +0000

    Add questions to admin panel

--- core/models.py
+++ core/models.py
@@ -7,3 +7,5 @@ class Contact(models.Model):
   description = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
 
+  def __unicode__(self):
+    return self.title
