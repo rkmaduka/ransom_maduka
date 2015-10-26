0755b734a2dbbb8fc33822c9441d6230b971c772
rkmaduka <rkmaduka@quinnipiac.edu>
2015-10-26 22:05:56 +0000

    Add styling to question form

--- core/views.py
+++ core/views.py
@@ -13,4 +13,8 @@ class ContactCreateView(CreateView):
     model = Contact
     template_name = "contact/contact_form.html"
     fields = ['name', 'email', 'description']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('success')
+
+
+class Success(TemplateView):
+  template_name = "success.html"
